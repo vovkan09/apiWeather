f24df7975b8ee605a7049c6d9084eb1d3b28fd43
Macbook <macbook@Macbooks-MacBook-Pro-Vladimir.local>
2022-06-25 00:33:55 +0300

    fixed JSON

--- apiWeather/apiWeather/ContentView.swift
+++ apiWeather/apiWeather/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var nameCity = ["Kazan", "Moscow", "London", "Perm", "Ekaterinburg"]
+    var nameCity = ["Kazan", "Moscow", "London", "Perm", "Anapa", "Sochi", "Ekaterinburg"]
     
     var body: some View {
         NavigationView {
@@ -44,8 +44,22 @@ struct ContentView: View {
                         RoundedRectangle(cornerRadius: 20)
                             .foregroundColor(.blue))
 				
+				NavigationLink {
+					OneCity3()
+				}
+				
+				label: {
+					Text(nameCity[2])
+				} .font(.largeTitle)
+					.foregroundColor(.white)
+					.frame(minWidth: 150, idealWidth: .infinity, maxWidth: .infinity, alignment: .top)
+					.frame(height: 80)
+					.background(
+						RoundedRectangle(cornerRadius: 20)
+							.foregroundColor(.blue))
+				
                 NavigationLink {
-                    OneCity3()
+                    OneCity4()
                 }
                 label: {
                    Text(nameCity[3])
@@ -56,7 +70,21 @@ struct ContentView: View {
                     .background(
                         RoundedRectangle(cornerRadius: 20)
                             .foregroundColor(.blue))
-
+				
+				NavigationLink {
+					OneCity5()
+				}
+				
+				label: {
+					Text(nameCity[4])
+				} .font(.largeTitle)
+					.foregroundColor(.white)
+					.frame(minWidth: 150, idealWidth: .infinity, maxWidth: .infinity, alignment: .top)
+					.frame(height: 80)
+					.background(
+						RoundedRectangle(cornerRadius: 20)
+							.foregroundColor(.blue))
+				
                
             }
             
@@ -69,16 +97,15 @@ struct OneCity: View {
     
     @State var info = WeatherInfo()
     
-    
     var body: some View {
                 VStack {
                     Text("\(Int(info.main.temp - 273)) C")
                     Spacer()
-                    Text(info.weather[0].main)
+                    Text("Conditions: \(info.weather[0].main)")
                     
 				} .frame(height: 200)
             .onAppear() {
-                Api().getPost { (info) in
+				Api() .getPost(nameCity: "kazan") { (info) in
                     self.info = info
                 }
             } .navigationBarTitle(info.name)
@@ -102,8 +129,9 @@ struct Weather: Codable {
 
 
 class Api {
-    func getPost(completion: @escaping(WeatherInfo) -> ()) {
-       guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=kazan&APPID=0ee73ee655b84ed02473641c7f2f44d4")
+	func getPost(nameCity: String, completion: @escaping(WeatherInfo) -> ()) {
+
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(nameCity)&APPID=0ee73ee655b84ed02473641c7f2f44d4")
         else{ return }
         
         URLSession.shared.dataTask(with: url) { (data, _, _) in
@@ -118,16 +146,87 @@ class Api {
 }
 
 struct OneCity2: View {
-    var body: some View {
-        Text("moscow")
-    }
+	
+	@State var info = WeatherInfo()
+	
+	var body: some View {
+				VStack {
+					Text("\(Int(info.main.temp - 273)) C")
+					Spacer()
+					Text("Conditions: \(info.weather[0].main)")
+					
+				} .frame(height: 200)
+			.onAppear() {
+				Api() .getPost(nameCity: "moscow") { (info) in
+					self.info = info
+				}
+			} .navigationBarTitle(info.name)
+		}
+		
+}
+
+struct OneCity3: View {
+	
+	@State var info = WeatherInfo()
+	
+	var body: some View {
+				VStack {
+					Text("\(Int(info.main.temp - 273)) C")
+					Spacer()
+					Text("Conditions: \(info.weather[0].main)")
+					
+				} .frame(height: 200)
+			.onAppear() {
+				Api() .getPost(nameCity: "london") { (info) in
+					self.info = info
+				}
+			} .navigationBarTitle(info.name)
+		}
+		
 }
 	
-struct OneCity3: View {
-    var body: some View {
-        Text("perm")
-    }
+struct OneCity4: View {
+	
+	@State var info = WeatherInfo()
+	
+	var body: some View {
+				VStack {
+					Text("\(Int(info.main.temp - 273)) C")
+					Spacer()
+					Text("Conditions: \(info.weather[0].main)")
+					
+				} .frame(height: 200)
+			.onAppear() {
+				Api() .getPost(nameCity: "perm") { (info) in
+					self.info = info
+				}
+			} .navigationBarTitle(info.name)
+		}
+		
 }
+
+struct OneCity5: View {
+	
+	@State var info = WeatherInfo()
+	
+	var body: some View {
+				VStack {
+					Text("\(Int(info.main.temp - 273)) C")
+					Spacer()
+					Text("Conditions: \(info.weather[0].main)")
+					
+				} .frame(height: 200)
+			.onAppear() {
+				Api() .getPost(nameCity: "anapa") { (info) in
+					self.info = info
+				}
+			} .navigationBarTitle(info.name)
+		}
+		
+}
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
